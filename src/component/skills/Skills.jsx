947162ab2fe3421e2c4caf9153a92e224bf14123
Himanshu Narware <himanshunarware77@gmail.com>
2025-03-26 18:31:51 +0530

    second

--- src/component/skills/Skills.jsx
+++ src/component/skills/Skills.jsx
@@ -2,10 +2,7 @@
 
 import React from 'react';
 import './Skills.css';
-
-
 import reacticon from '../../assets/react.svg';
-// calling all icons
 import html from '../../assets/html.svg';
 import css from '../../assets/css.svg';
 import javascript from '../../assets/js.svg';
@@ -27,19 +24,17 @@ import java from '../../assets/java.svg';
 import nextjs from '../../assets/nextjs.svg';
 import python_2 from '../../assets/python-2.svg';
 import sql_2 from '../../assets/sql-2.svg';
-import bootstrap from '../../assets/bootstrap.svg';
-import postman from '../../assets/postman.svg';
+import bootstrap from '../../assets/Bootstrap.svg';
+import postman from '../../assets/Postman.svg';
 import express from '../../assets/expressjs-ar21.svg';
-import linux from '../../assets/linux.svg';
+import linux from '../../assets/Linux.svg';
 import githubActions from '../../assets/GitHub Actions.svg';
 import sql from '../../assets/sql.svg';
-import insomnia from '../../assets/insomnia.svg';
+import insomnia from '../../assets/Insomnia.svg';
 import photoshop from '../../assets/Adobe photoshop.svg';
-const dummyImage = 'https://placehold.co/10/transparent/FF00';
+// const dummyImage = 'https://placehold.co/10/transparent/FF00';
 
 const techStack = [
-  // { id: 1, src: dummyImage },
-  // { id: 1, src: dummyImage },
   { id: 1, src: reacticon, alt: 'React' },
   { id: 2, src: nodejs, alt: 'Vue' },
   { id: 3, src: express, alt: 'Angular' },
@@ -48,9 +43,7 @@ const techStack = [
   { id: 3, src: java, alt: 'Angular' },
   { id: 3, src: aws_1, alt: 'Angular' },
   { id: 3, src: tailwind, alt: 'Angular' },
-  //  { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
+
   { id: 3, src: css, alt: 'Angular' },
   { id: 3, src: html, alt: 'Angular' },
   { id: 3, src: javascript, alt: 'Angular' },
@@ -64,44 +57,30 @@ const techStack = [
   { id: 3, src: linux, alt: 'Angular' },
   { id: 3, src: photoshop, alt: 'Angular' },
   { id: 3, src: githubActions, alt: 'Angular' },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
+
   { id: 3, src: python_2, alt: 'Angular' },
   { id: 3, src: sass, alt: 'Angular' },
   { id: 3, src: nextjs, alt: 'Angular' },
   { id: 3, src: sql_2, alt: 'Angular' },
   { id: 3, src: bootstrap, alt: 'Angular' },
   { id: 3, src: postman, alt: 'Angular' },
-  //   { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
+
   { id: 3, src: angular, alt: 'Angular' },
   { id: 3, src: insomnia, alt: 'Angular' },
   { id: 3, src: sql, alt: 'Angular' },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-  // { id: 0, src: dummyImage },
-
-  // ...add other icons here
 ];
 
 const Skills = () => {
   return (
     <>
-      <div id='skills' className="bg-[#101010] text-center linear text-white protest-riot-regular text-5xl font-color ">
+      <div
+        id="skills"
+        className="bg-[#101010] text-center linear text-white protest-riot-regular text-5xl font-color ">
         Technical Skills
       </div>
       <div className="main-container max-md:min-h-[60-wh] min-h-[80vh] bg-[#101010] flex items-center justify-center p-6">
         <div className="grid grid-cols-5 gap-6 md:grid-cols-6 lg:grid-cols-10 max-md:grid-cols-4 max-md:gap-4 ">
-          {techStack.map((tech,index) => (
+          {techStack.map((tech, index) => (
             <div
               max-md:gap-4
               key={index}
