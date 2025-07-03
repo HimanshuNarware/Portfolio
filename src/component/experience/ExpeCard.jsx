89eb4b0f2188dc0a36eb8ae90b767edfec50809f
Himanshu Narware <himanshunarware77@gmail.com>
2025-07-03 21:11:29 +0530

    image arr

--- src/component/experience/ExpeCard.jsx
+++ src/component/experience/ExpeCard.jsx
@@ -1,38 +1,47 @@
-import React from 'react'
-function ExpeCard({data}) {
-  // console.log(data)
+/** @format */
+
+import React from 'react';
+import kwoc from '../../assets/experience/kwoc.png';
+import dwoc from '../../assets/experience/dwoc.png';
+import gssoc from '../../assets/experience/gssoc.png';
+import hack from '../../assets/experience/hack.png';
+import slop from '../../assets/experience/slop.png';
+import swoc from '../../assets/experience/swoc.png';
+import woos from '../../assets/experience/woos.png';
+import gdsc from '../../assets/experience/gdsc.png';
+
+function ExpeCard({ data, index }) {
+  const images = [woos, gssoc, gdsc, gssoc, slop, dwoc, swoc, kwoc, hack];
 
   return (
-    <div
-   
-    className="bg-[#181818]  rounded-2xl p-6 shadow-xl flex flex-col gap-4"
-  >
-    {/* Icon */}
-    <div className="flex items-center gap-4 ">
-      
-        <img className='className="w-30 h-20 rounded flex justify-center items-center' src={data.image} alt="" />
-      
-      <div className="flex flex-col  gap-2 ">
-        <button className="px-3 py-1 text-xs bg-[#2c2c2c] text-white rounded-md border border-gray-700">
-          {data.positions[0].title}
-        </button>
-        <button className="px-3 py-1 text-xs bg-[#2c2c2c] text-white rounded-md border border-gray-700">
-        {data.positions[0].duration}
-        </button>
-      </div>
-    </div>
+    <div className="bg-[#181818]  rounded-2xl p-6 shadow-xl flex flex-col gap-4">
+      {/* Icon */}
+      <div className="flex items-center gap-4 ">
+        <img
+          className='className="w-30 h-20 rounded flex justify-center items-center'
+          src={images[index]}
+          alt=""
+        />
 
-    {/* Content */}
-    <h3 className="text-white font-semibold text-lg">
-      {data.organization}
-    </h3>
-    <p className="text-gray-400 text-sm leading-relaxed">
-    {data.positions[0].description[0]}
-      {/* Description-It's never been easier to setup custom SSR (Server-Side Rendering), 
+        <div className="flex flex-col  gap-2 ">
+          <button className="px-3 py-1 text-xs bg-[#2c2c2c] text-white rounded-md border border-gray-700">
+            {data.positions[0].title}
+          </button>
+          <button className="px-3 py-1 text-xs bg-[#2c2c2c] text-white rounded-md border border-gray-700">
+            {data.positions[0].duration}
+          </button>
+        </div>
+      </div>
+
+      {/* Content */}
+      <h3 className="text-white font-semibold text-lg">{data.organization}</h3>
+      <p className="text-gray-400 text-sm leading-relaxed">
+        {data.positions[0].description[0]}
+        {/* Description-It's never been easier to setup custom SSR (Server-Side Rendering), 
       or build your own SSR framework. */}
-    </p>
-  </div>
-  )
+      </p>
+    </div>
+  );
 }
 
-export default ExpeCard
+export default ExpeCard;
--- src/component/experience/Experience.jsx
+++ src/component/experience/Experience.jsx
@@ -17,7 +17,7 @@ function Experience() {
       {/* Cards Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-6xl">
     
-    {experienceDb.experience.map((item)=><ExpeCard data={item}/>)}
+    {experienceDb.experience.map((item,index)=><ExpeCard key={index} data={item} index={index}/>)}
 
       </div>
 
